2623729f20c3bce11139eacfb130d689ebd3a312
Ali1bra <141811628+Ali1bra@users.noreply.github.com>
2026-03-24 11:59:29 +0100

    seo og tilføjet nyt billede til galleri

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -3,25 +3,30 @@ const galleryContent = document.querySelector(".content")
 
 const pictures = [
     {
+        url: "gallery-images/kunde6.jpg",
+        alt: "Skin fade klipning hos det gyldne snit / dgsbarber"
+    },
+    {
         url: "gallery-images/kunde5.jpg",
-        alt: "Klassisk klipning"
+        alt: "Klassisk klipning hos det gyldne snit / dgsbarber"
     },
     {
         url: "gallery-images/kunde4.jpg",
-        alt: "Skin fade klipning"
+        alt: "Skin fade klipning hos det gyldne snit / dgsbarber"
     },
     {
         url: "gallery-images/kunde1.jpg",
-        alt: "Klassisk klipning"
+        alt: "Klassisk klipning hos det gyldne snit / dgsbarber"
     },
     {
         url: "gallery-images/kunde3.jpg",
-        alt: "Klassisk klipning"
+        alt: "Klassisk klipning hos det gyldne snit / dgsbarber"
     },
     {
         url: "gallery-images/kunde2.jpg",
-        alt: "Klassisk klipning"
+        alt: "Klassisk klipning hos det gyldne snit / dgsbarber"
     },
+    
   
 
 ]
